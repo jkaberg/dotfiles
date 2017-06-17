c6794d12ad91d9adf0a5c4595dd400cbd0e51667
Joel Kåberg <joel.kaberg@nhn.no>
2017-06-17 13:07:04 +0200

    powerline!

---
+++ install.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Link files and dirs
+files=(".bash_include" ".bashrc" "bin" ".gitconfig" ".tmux.conf" ".vimrc")
+for f in "${files[@]}"
+do
+    :
+    path="$PWD/$f"
+    rm -rf "~/$f"
+
+    if [[ -d "$path" ]]; then
+        ln -s "$path" "~/$f"
+    else
+        ln "$path" "~/$f"
+    fi
+done
+
+# Powerline install
+pip install --user git+git://github.com/powerline/powerline
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+mkdir -p ~/.fonts/ && mv PowerlineSymbols.otf ~/.fonts/
+fc-cache -vf ~/.fonts
+mkdir -p ~/.config/fontconfig/conf.d/ && mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+
+
